5ceff9985b5d87fbf0926bbe10837b09ad6a7b12
Ilyas Aminov <aminov.i.g@yandex.ru>
2023-05-31 01:11:47 +0300

    Solution task about factorial

---
+++ factorial/Program.cs
@@ -0,0 +1,21 @@
+﻿// Нахождение факториала
+
+double Factorial(int n)
+{
+    if(n == 1)
+    {
+        return 1;
+    }
+    else 
+    {
+        return n * Factorial(n - 1);
+    }
+}
+
+for(int i = 1; i < 18; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+
+// при значении i более 16 происходит переполнение типа данных integer
+// Справиться с этой проблемой можно путём смены типа данных (просто поменять тип метода int на double)
